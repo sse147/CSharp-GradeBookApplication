e7f49dbfc115dc82d59fa162046a5831b56772a1
Paul Jewkes <paul.jewkes@sse.com>
2022-05-26 14:01:38 +0100

    "override statistics" correct

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,6 +16,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
 
             base.CalculateStatistics();
@@ -26,6 +27,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
 
             base.CalculateStudentStatistics(name);
